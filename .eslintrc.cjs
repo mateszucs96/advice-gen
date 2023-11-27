251a5c6157a58339bf7cd7c4621d2f550ff1bd97
mateszucs96 <szucsmate096@gmail.com>
2023-11-27 16:38:02 +0100

    playing around with fetching data

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		indent: ['error', 'tab'],
+		'react/react-in-jsx-scope': 'off',
+		'linebreak-style': ['error', 'windows'],
+		'no-mixed-spaces-and-tabs': 0,
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [advice, setAdvice] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const fetchData = async () => {
+		setIsLoading(true);
+		const res = await fetch('https://api.adviceslip.com/advice');
+		const data = await res.json();
+		setAdvice(data.slip);
+		setIsLoading(false);
+		console.log(data.slip);
+	};
+
+	const giveAdvice = () => {
+		fetchData();
+		console.log('clciked');
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<div>
+					<p className="read-the-docs">{advice.id}</p>
+					<p className="read-the-docs">{advice.advice}</p>
+					<button onClick={giveAdvice}>click</button>
+				</div>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
